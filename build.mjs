dfbb76a4c89aef7c294e600aca6b27e6abbe4cf6
lastknighcoder <2223106858@qq.com>
2022-05-23 20:29:57 +0800

    svg

--- build.mjs
+++ build.mjs
@@ -12,6 +12,7 @@ const __dirname = path.dirname(__filename)
   const files = await fs.readdir(__dirname)
   const dirs = []
 
+  // 筛选出所有包含 slides.md 的文件夹，并进行 build
   for (let file of files) {
     const stat = await fs.stat(path.join(__dirname, file))
     if (!stat.isDirectory()) continue
@@ -25,8 +26,6 @@ const __dirname = path.dirname(__filename)
   }
 
   for (let dir of dirs) {
-    await $`pwd`
     await $`npx slidev build ./${dir}/slides.md --base /slides/${dir}/ --out ../slides/flex`
   }
-  
 })()
