669b72652c58a76793e95fd040c92c3bda331c44
lastknighcoder <2223106858@qq.com>
2022-06-18 14:39:11 +0800

    paper finish

---
+++ end-paper/components/MultiClassResult.vue
@@ -0,0 +1,38 @@
+<template>
+  <el-tabs type="card" style="margin-top: 1rem;">
+    <el-tab-pane label="带噪训练数据">
+      <slot name="noise-results"></slot>
+    </el-tab-pane>
+    <el-tab-pane label="带噪检测效果">
+      <div class="img-grid">
+        <img src="/detect-results/m1.jpg" style="zoom: 100%;" />
+        <img src="/detect-results/m2.jpg" style="zoom: 100%;" />
+        <img src="/detect-results/m3.jpg" style="zoom: 100%;" />
+        <img src="/detect-results/m4.jpg" style="zoom: 100%;" />
+      </div>
+    </el-tab-pane>
+    <el-tab-pane label="去噪训练数据">
+      <slot name="denoise-results"></slot>
+    </el-tab-pane>
+    <el-tab-pane label="去噪检测效果">
+      <div class="img-grid">
+        <img src="/detect-results/m5.png" style="zoom: 100%;" />
+        <img src="/detect-results/m6.png" style="zoom: 100%;" />
+        <img src="/detect-results/m7.png" style="zoom: 100%;" />
+        <img src="/detect-results/m8.png" style="zoom: 100%;" />
+      </div>
+    </el-tab-pane>
+  </el-tabs>
+</template>
+
+<script setup>
+import { ElTabs, ElTabPane } from 'element-plus'
+</script>
+
+<style scoped>
+.img-grid {
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  gap: 10px;
+}
+</style>
